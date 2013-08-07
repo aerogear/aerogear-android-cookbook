cb707c6834cbd8c93a0119ae702a5cddcff612b6
Summers Pittman <secondsun@gmail.com>
2013-08-07 13:13:57 -0400

    Updating Push Activity to include implementation.

--- src/org/jboss/aerogear/cookbook/GuideApplication.java
+++ src/org/jboss/aerogear/cookbook/GuideApplication.java
@@ -1,16 +1,29 @@
 package org.jboss.aerogear.cookbook;
 
 import android.app.Application;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.jboss.aerogear.android.DataManager;
 import org.jboss.aerogear.android.impl.datamanager.MemoryStorage;
 import org.jboss.aerogear.android.impl.datamanager.StoreConfig;
 import org.jboss.aerogear.android.impl.datamanager.StoreTypes;
+import org.jboss.aerogear.android.unifiedpush.PushConfig;
+import org.jboss.aerogear.android.unifiedpush.PushRegistrar;
+import org.jboss.aerogear.android.unifiedpush.Registrations;
 import org.jboss.aerogear.cookbook.datamanager.IncrementalIdGenerator;
 import org.jboss.aerogear.cookbook.model.Car;
+import static org.jboss.aerogear.cookbook.push.PushConstants.GCM_SENDER_ID;
+import static org.jboss.aerogear.cookbook.push.PushConstants.MY_ALIAS;
+import static org.jboss.aerogear.cookbook.push.PushConstants.SECRET;
+import static org.jboss.aerogear.cookbook.push.PushConstants.UNIFIED_PUSH_URL;
+import static org.jboss.aerogear.cookbook.push.PushConstants.VARIANT_ID;
 
 public class GuideApplication extends Application {
 
     private MemoryStorage memoryStorage;
+    private PushRegistrar registrar;
+    private PushConfig config;
+    private final Registrations registrations = new Registrations();
 
     @Override
     public void onCreate() {
@@ -44,4 +57,27 @@ public class GuideApplication extends Application {
         return memoryStorage;
     }
 
+    public void enablePush() {
+
+        if (registrar == null) {
+            try {
+                config = new PushConfig(new URL(UNIFIED_PUSH_URL), GCM_SENDER_ID);
+                config.setVariantID(VARIANT_ID);
+                config.setSecret(SECRET);
+                config.setAlias(MY_ALIAS);
+
+                registrar = registrations.push("registrar", config);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+    
+    public PushRegistrar getRegistrar() {
+        if (registrar == null) {
+            enablePush();
+        }
+        return registrar;
+    }
 }
