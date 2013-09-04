cd4ee361c9a2066b528fe10fefa56bc5e8aaef59
Summers Pittman <secondsun@gmail.com>
2013-09-04 15:54:56 -0400

    Migrating to using a Fragment for Android Gingerbread

--- src/org/jboss/aerogear/cookbook/pipeline/HowToUseCustomPipe.java
+++ src/org/jboss/aerogear/cookbook/pipeline/HowToUseCustomPipe.java
@@ -16,72 +16,24 @@
  */
 package org.jboss.aerogear.cookbook.pipeline;
 
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.widget.Toast;
-import org.jboss.aerogear.android.Pipeline;
-import org.jboss.aerogear.android.impl.pipeline.PipeConfig;
-import org.jboss.aerogear.android.pipeline.AbstractActivityCallback;
-import org.jboss.aerogear.android.pipeline.LoaderPipe;
-import org.jboss.aerogear.android.pipeline.Pipe;
-import org.jboss.aerogear.cookbook.CarAdapter;
 import org.jboss.aerogear.cookbook.R;
-import org.jboss.aerogear.cookbook.model.Car;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 
-public class HowToUseCustomPipe extends ListActivity {
-
-    private Pipe<Car> carsPipe;
+public class HowToUseCustomPipe extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.car_list);
-
-        // Create/Config Pipeline
-        try {
-            URL fileURL = getFilesDir().toURI().toURL();
-            Pipeline pipeline = new Pipeline(fileURL);
-            PipeConfig fileReaderConfig = new PipeConfig(fileURL, Car.class);
-            fileReaderConfig.setHandler(new FileHandler(this));
-            pipeline.pipe(Car.class, fileReaderConfig);
-
-            // Retrive Pipe
-            carsPipe = (LoaderPipe<Car>) pipeline.get("car", this);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        carsPipe.read(new FileCallback());
-    }
-
-    private static final class FileCallback extends AbstractActivityCallback<List<Car>> {
-
-        private static final long serialVersionUID = 1L;
-
-        public FileCallback() {
-            super("hashableValue");
-        }
-
-        @Override
-        public void onSuccess(List<Car> data) {
-            ListActivity activity = (ListActivity) getActivity();
-            activity.setListAdapter(new CarAdapter(activity, data));
-        }
-
-        @Override
-        public void onFailure(Exception e) {
-            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ListFragment fragment = new HowToUseCustomPipeFragment();
+        ft.add(R.id.list, fragment);
+        ft.commit();
+        
     }
 
 }
