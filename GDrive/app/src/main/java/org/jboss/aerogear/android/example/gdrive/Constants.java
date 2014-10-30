dbae25698d6d7752ab210e97d1f3c9ee576df24d
Daniel Passos <daniel@passos.me>
2014-10-30 12:11:55 -0200

    Remove my credentials

--- GDrive/app/src/main/java/org/jboss/aerogear/android/example/gdrive/Constants.java
+++ GDrive/app/src/main/java/org/jboss/aerogear/android/example/gdrive/Constants.java
@@ -6,8 +6,8 @@ public interface Constants {
     String AUTHZ_ENDPOINT = "/o/oauth2/auth";
     String AUTHZ_TOKEN_ENDPOINT = "/o/oauth2/token";
     String AUTHZ_ACCOUNT_ID = "ag-authz";
-    String AUTHZ_CLIENT_ID = "37850448940-bqahddol8a694oadlqnj4n2el4kr5pqk.apps.googleusercontent.com";
-    String AUTHZ_CLIENT_SECRET = "6F6pPjHQT29qwvqvxzNv_Ks7";
-    String AUTHZ_REDIRECT_URL = "http://localhost";
+    String AUTHZ_CLIENT_ID = "";
+    String AUTHZ_CLIENT_SECRET = "";
+    String AUTHZ_REDIRECT_URL = "";
 
 }
