d0195544f2b0b4f1d775ab9824f8bcf84bbba268
Daniel Passos <daniel@passos.me>
2016-11-03 15:00:00 -0200

    Remove unused import

--- ChuckNorrisJokes/app/src/main/java/org/jboss/aerogear/android/cookbook/chucknorrisjokes/ui/ChuckNorrisJokesActivity.java
+++ ChuckNorrisJokes/app/src/main/java/org/jboss/aerogear/android/cookbook/chucknorrisjokes/ui/ChuckNorrisJokesActivity.java
@@ -3,7 +3,6 @@ package org.jboss.aerogear.android.cookbook.chucknorrisjokes.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
