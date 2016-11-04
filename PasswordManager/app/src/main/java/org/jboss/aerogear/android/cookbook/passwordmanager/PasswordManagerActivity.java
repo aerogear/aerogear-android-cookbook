32922fc2e8c4ecfc6439bd7a0b41c2f41afd21a9
Daniel Passos <daniel@passos.me>
2016-11-04 12:57:38 -0200

    Replace ActionBarActivity by AppCompatActivity

--- PasswordManager/app/src/main/java/org/jboss/aerogear/android/cookbook/passwordmanager/PasswordManagerActivity.java
+++ PasswordManager/app/src/main/java/org/jboss/aerogear/android/cookbook/passwordmanager/PasswordManagerActivity.java
@@ -1,13 +1,13 @@
 /**
  * JBoss, Home of Professional Open Source
  * Copyright Red Hat, Inc., and individual contributors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- * 	http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@ package org.jboss.aerogear.android.cookbook.passwordmanager;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import org.jboss.aerogear.android.cookbook.passwordmanager.fragments.DetailFragment;
@@ -29,10 +29,7 @@ import org.jboss.aerogear.android.cookbook.passwordmanager.fragments.LoginFragme
 import org.jboss.aerogear.android.cookbook.passwordmanager.model.Credential;
 import org.jboss.aerogear.android.security.InvalidKeyException;
 
-import java.security.spec.InvalidKeySpecException;
-import java.util.Collection;
-
-public class PasswordManagerActivity extends ActionBarActivity {
+public class PasswordManagerActivity extends AppCompatActivity {
 
     private PasswordManagerApplication application;
 
