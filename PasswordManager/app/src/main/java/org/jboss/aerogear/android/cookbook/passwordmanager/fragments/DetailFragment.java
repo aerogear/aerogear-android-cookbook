15cde0a1cfa6303f668d7442499716e9cf912636
Daniel Passos <daniel@passos.me>
2016-11-04 12:56:01 -0200

    Remove copy to clipboard

--- PasswordManager/app/src/main/java/org/jboss/aerogear/android/cookbook/passwordmanager/fragments/DetailFragment.java
+++ PasswordManager/app/src/main/java/org/jboss/aerogear/android/cookbook/passwordmanager/fragments/DetailFragment.java
@@ -1,13 +1,13 @@
 /**
  * JBoss, Home of Professional Open Source
  * Copyright Red Hat, Inc., and individual contributors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- * 	http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,19 +17,14 @@
 package org.jboss.aerogear.android.cookbook.passwordmanager.fragments;
 
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.jboss.aerogear.android.cookbook.passwordmanager.R;
 import org.jboss.aerogear.android.cookbook.passwordmanager.model.Credential;
-
-import java.io.Serializable;
 
 public class DetailFragment extends Fragment {
 
@@ -48,15 +43,6 @@ public class DetailFragment extends Fragment {
         TextView username = (TextView) view.findViewById(R.id.username);
         username.setText(credential.getUsername());
 
-        ImageView clipboard = (ImageView) view.findViewById(R.id.clipboard);
-        clipboard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                copyToClipboard(credential.getPassword());
-                Toast.makeText(getActivity(), getString(R.string.copied_clipboard), Toast.LENGTH_SHORT).show();
-            }
-        });
-
         final TextView password = (TextView) view.findViewById(R.id.password);
         password.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,17 +59,5 @@ public class DetailFragment extends Fragment {
         return view;
     }
 
-    private void copyToClipboard(String password) {
-        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
-            android.text.ClipboardManager clipboard = (android.text.ClipboardManager)
-                    getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
-            clipboard.setText(password);
-        } else {
-            android.content.ClipboardManager clipboard = (android.content.ClipboardManager)
-                    getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
-            android.content.ClipData clip = android.content.ClipData.newPlainText(getString(R.string.password), password);
-            clipboard.setPrimaryClip(clip);
-        }
-    }
 
 }
