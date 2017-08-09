0b5a611e3a4c82fa510d9256c906f9f99cc9e5b2
damien <damurphy@redhat.com>
2017-08-09 12:24:55 +0100

    adds notification badge func.

--- HelloPush/app/src/main/java/org/jboss/aerogear/android/cookbook/hellopush/handler/NotificationBarMessageHandler.java
+++ HelloPush/app/src/main/java/org/jboss/aerogear/android/cookbook/hellopush/handler/NotificationBarMessageHandler.java
@@ -16,12 +16,17 @@
  */
 package org.jboss.aerogear.android.cookbook.hellopush.handler;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 
 import org.jboss.aerogear.android.unifiedpush.MessageHandler;
@@ -30,9 +35,13 @@ import org.jboss.aerogear.android.cookbook.hellopush.HelloWorldApplication;
 import org.jboss.aerogear.android.cookbook.hellopush.R;
 import org.jboss.aerogear.android.cookbook.hellopush.activities.MessagesActivity;
 
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+
 public class NotificationBarMessageHandler implements MessageHandler {
 
     public static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "hellopush_channel";
+    private Context context;
 
     public static final NotificationBarMessageHandler instance = new NotificationBarMessageHandler();
 
@@ -41,12 +50,49 @@ public class NotificationBarMessageHandler implements MessageHandler {
 
     @Override
     public void onMessage(Context context, Bundle bundle) {
+
+        this.context = context;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+        }
+
         String message = bundle.getString(UnifiedPushMessage.ALERT_KEY);
 
         HelloWorldApplication application = (HelloWorldApplication) context.getApplicationContext();
         application.addMessage(message);
 
         notify(context, bundle);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            throw new IllegalStateException("This function should not be called on < Android O");
+        }
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // The user-visible name of the channel.
+        CharSequence name = context.getString(R.string.channel_name);
+
+        // The user-visible description of the channel.
+        String description = context.getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+        // Configure the notification channel.
+        channel.setDescription(description);
+        channel.enableLights(true);
+
+        // Sets the notification light color for notifications posted to this
+        // channel, if the device supports this feature.
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+        channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(channel);
+
     }
 
     private void notify(Context context, Bundle bundle) {
@@ -56,6 +102,7 @@ public class NotificationBarMessageHandler implements MessageHandler {
         String message = bundle.getString(UnifiedPushMessage.ALERT_KEY);
         String pushMessageId = bundle.getString(UnifiedPushMessage.PUSH_MESSAGE_ID);
 
+        @SuppressLint("WrongConstant")
         Intent intent = new Intent(context, MessagesActivity.class)
                 .addFlags(PendingIntent.FLAG_UPDATE_CURRENT)
                 .putExtra(UnifiedPushMessage.ALERT_KEY, message)
@@ -63,9 +110,9 @@ public class NotificationBarMessageHandler implements MessageHandler {
                 .putExtra(HelloWorldApplication.PUSH_MESSAGE_FROM_BACKGROUND, true);
 
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+                FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(context.getString(R.string.app_name))
